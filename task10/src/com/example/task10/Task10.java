0de0eb93a278fab2f739ea6a53d1e849530b8ab1
Balandala <balandala05@gmail.com>
2024-09-20 03:59:08 +0500

    Раскостов Павел Task010

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -3,13 +3,10 @@ package com.example.task10;
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
-
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+    if  (Double.isNaN(a - b)) // a - b == NaN может быть только при Inf - Inf или (a || b == NaN)
+        return Double.isNaN(a) && Double.isNaN(b) || Double.isInfinite(a) && Double.isInfinite(b);
+    else
+        return Math.abs(a - b) <  Math.pow(0.1, Math.max(precision, 1));
     }
 
     public static void main(String[] args) {
