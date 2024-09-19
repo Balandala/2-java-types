9f55009d853a51d1ecc9c3edfb24e22da97192b6
Balandala <balandala05@gmail.com>
2024-09-20 03:58:32 +0500

    Раскостов Павел Task06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -1,12 +1,12 @@
 package com.example.task06;
 
+
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        String str = Integer.toString(x + y);
+        str = str.replaceAll("-", "");
+        return str.length();
     }
 
     public static void main(String[] args) {
